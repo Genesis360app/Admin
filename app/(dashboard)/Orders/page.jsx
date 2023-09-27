60f02ca896cb5925c2c94bfa5793cf6b4c1c89ee
tomcyrus <tomcyrus0@gmail.com>
2023-09-27 03:21:38 +0100

    update

--- app/(dashboard)/Orders/page.jsx
+++ app/(dashboard)/Orders/page.jsx
@@ -1,25 +1,25 @@
-"use client"
+// "use client"
 
-// pages/page.jsx
+// // pages/page.jsx
 
-import { useRouter } from 'next/navigation'
+// import { useRouter } from 'next/navigation'
 
 
-function Page() {
-  const router = useRouter();
-  const { itemId } = router.query; // Access the itemId from the router query
+// function Page() {
+//   const router = useRouter();
+//   const { itemId } = router.query; // Access the itemId from the router query
 
-  // You can fetch data related to the item using the itemId
-  // For example, you can make an API request to get item details
+//   // You can fetch data related to the item using the itemId
+//   // For example, you can make an API request to get item details
 
-  return (
-    <div>
-      <h1>Item Page</h1>
-      <p>Item ID: {itemId}</p>
-      {/* Display other item details based on the fetched data */}
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h1>Item Page</h1>
+//       <p>Item ID: {itemId}</p>
+//       {/* Display other item details based on the fetched data */}
+//     </div>
+//   );
+// }
 
-export default Page;
+// export default Page;
 
